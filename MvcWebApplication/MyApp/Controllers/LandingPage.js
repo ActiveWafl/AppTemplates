435862229a8cc4f0dc18c7712073173bb90c1088
Evan <Evan@EvanThinkpad440.ph.cox.net>
2014-08-11 11:45:48 -0700

    don't use start in client-side controller actions.  we're always past start already at this point

--- MvcWebApplication/MyApp/Controllers/LandingPage.js
+++ MvcWebApplication/MyApp/Controllers/LandingPage.js
@@ -1,22 +1,27 @@
-Start(function() {
-    DblEj.EventHandling.Events.AddHandler(window, "hashchange", function(e)
-    {
-        SetMenuClassFromUrl();
-    });
+Namespace("ActiveWaflWeb.Controllers.Ui");
 
-    SetMenuClassFromUrl();
-});
-
-function SetMenuClassFromUrl()
-{
-    $$q('#PageMenu a')
-        .RemoveClass('Selected');
-    if (window.location.hash.length == 0)
+ActiveWaflWeb.Controllers.Ui.Components = DblEj.Mvc.ControllerBase.extend({
+    init: function()
     {
-        $("MenuInstallComplete")
-            .AddClass('Selected');
-    } else {
-        $("Menu" + window.location.hash.substring(1))
-            .AddClass('Selected');
+        this._setMenuClassFromUrl = function()
+        {
+            $$q('#PageMenu a')
+                .RemoveClass('Selected');
+            if (window.location.hash.length == 0)
+            {
+                $("MenuInstallComplete")
+                    .AddClass('Selected');
+            } else {
+                $("Menu" + window.location.hash.substring(1))
+                    .AddClass('Selected');
+            }
+        };              
+    },
+	DefaultAction: function()
+	{
+        DblEj.EventHandling.Events.AddHandler(window, "hashchange", function(e)
+        {
+            this._setMenuClassFromUrl();
+        }.Bind(this));
     }
-}
+});
