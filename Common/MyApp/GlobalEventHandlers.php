acd2dc41867854c5ff2a8f739c45a76341e0fd9a
Evan <Evan@EvanThinkpad440.ph.cox.net>
2014-11-20 11:53:41 -0700

    Set default read/write access methods for client side model persistence which asks the model instance if read/write operation is permitted

--- Common/MyApp/GlobalEventHandlers.php
+++ Common/MyApp/GlobalEventHandlers.php
@@ -4,5 +4,19 @@ DblEj\Util\SystemEvents::AddSystemHandler(DblEj\Util\SystemEvents::AFTER_INITIAL
     $traceHandler = new \DblEj\Logging\PhpLogTraceHandler();
     $traceHandler->SetOption("PhpLogType", \DblEj\Logging\PhpLogTraceHandler::LOGTYPE_SYSTEM);
     \DblEj\Logging\Tracing::SetTraceHandler($traceHandler);
+
+    //setup clientside model load/save security
+    Wafl\Api\DataModelHandlers::SetAuthenticateReadAccessMethod(AuthenticateModelRead);
+    Wafl\Api\DataModelHandlers::SetAuthenticateWriteAccessMethod(AuthenticateModelWrite);
+
+    function AuthenticateModelRead(\DblEj\Data\PersistableModel $model)
+    {
+        return $model->DoesCurrentUserHaveReadAccess();
+    }
+
+    function AuthenticateModelWrite(\DblEj\Data\PersistableModel $model)
+    {
+        return $model->DoesUserHaveWriteAccess();
+    }
 });
 ?>
