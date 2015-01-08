063e80f329d23d9c97488535edae0b92691326c4
Evan <Evan@EvanThinkpad440.ph.cox.net>
2015-01-08 11:45:24 -0700

    turn on default trace handling

--- CommonWeb/MyApp/GlobalScript.js
+++ CommonWeb/MyApp/GlobalScript.js
@@ -1,4 +1,4 @@
 Start(function()
 {
-    //Global js script here
+    DblEj.Logging.Tracing.AddTraceHandler(new DblEj.Logging.BrowserConsoleTraceHandler(DblEj.Logging.Tracing.LOGLEVEL_WARNING), DblEj.Logging.Tracing.LOGLEVEL_WARNING);
 });
