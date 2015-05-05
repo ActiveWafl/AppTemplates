64f8640f2baa6a2454da0a372f896bbeff98498c
Evan <Evan@192.168.1.104>
2015-05-05 01:59:46 -0700

    Remove AppSupport.phar dependency

--- ModularMvcWebApplication/Public/index.php
+++ ModularMvcWebApplication/Public/index.php
@@ -2,71 +2,65 @@
 use {APPLICATION_NAMESPACE}\ModularLoader,
     Wafl\AppSupport\WebIndex,
     Wafl\Util\HttpRouter;
-
-//WAFL web application support file
-require_once(__DIR__."/../AppSupport.phar");
-
-//This is here to warn you of configuration errors
-//It can be deleted
-$requestDomain = $_SERVER["SERVER_NAME"];
-if (strtolower($requestDomain) != strtolower($application->Get_Settings()->Get_Web()->Get_DomainName()))
+try
 {
-    throw new Wafl\Application\Settings\InvalidSettingException("DomainName","The domain name that you have set in Settings.".WAFL_ENVIRONMENT.".syrp (".$application->Get_Settings()->Get_Web()->Get_DomainName().") does not match the domain name in the current request URL ($requestDomain).  Please specify the correct domain or access the page through the specified URL.");
-}
+    require_once(__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Application.php");
 
-//Get the requested URI so we know what to do
-$requestUri		 = $_SERVER["REQUEST_URI"];
-
-//once rendered, dynamic resources such as dynamic images and dynamic scripts can be cached on disk in their static state for improved performance
-//This is important in WAFL as many of these resources are generated dynamically
-if (WebIndex::OutputCachedStaticResources($requestUri))
-{
-    $exitCode = 0;
-} else {
-
-    //Initialize the application
-    $application = WebIndex::BootstrapModularApplication(__DIR__."/../Application.syrp",null,"\\Wafl\\Application\\ModularMvcWebApplication");
-
-    //delete expired caches
-    WebIndex::UncacheStaticResources(null, true);
-
-    //give all of the routers an opportunity to handle the request right away, before executing the application logic
-    //this improves performance for those requests that do not require the application logic
-    HttpRouter::RouteThruNonAppCalls($requestUri, $preAppRouter);
-
-    if ($preAppRouter !== null)
+    if (WebIndex::OutputCachedStaticResources($requestUri)) //static resources such as images and scripts can be cached on disk
     {
-        //If a router handled the request in the pre-routing stage,
-        //and it returned a dynamically generated resource such as an image or a script
-        //then cache its response, so that next time we can just serve it in its already-compiled form.
-        WebIndex::CacheRawStaticResource($requestUri, $preappRawOutput, $preappOutputHash);
-
-        //set the exit code to indicate success
-        $exitCode = 0;
+        exit(0);
     } else {
+        $application  = WebIndex::BootstrapModularApplication(__DIR__ . DIRECTORY_SEPARATOR ."..".DIRECTORY_SEPARATOR."Application.syrp", null, "\\Wafl\\Application\\ModularMvcWebApplication");
 
-        //If a router did not handle the request in the pre-routing stage,
-        //then run the full application, which will route the request and return the result.
-        //(it will also output any generated HTML to the browser along with appropriate HTTP headers)
-        $result = WebIndex::RunModularApplication($application, new ModularLoader(), $exitCode);
-
-        if ($result->Get_ResponseCode() == \DblEj\Communication\Http\Response::HTTP_OK_200)
+        if ($_SERVER["REQUEST_SCHEME"] == "http")
         {
-
-            //If the application returned a dynamically generated resource such as an image or a script
-            //then cache its response, so that next time we can just serve it in its already-compiled form.
-            WebIndex::CacheStaticResource($requestUri, $result);
-
-            //set the exit code to indicate success
-            $exitCode = 0;
-        } else {
-            //set the exit code to indicate a non-normal response
-            $exitCode = -1;
+            $domainName = $_SERVER["HTTP_HOST"];
+            \DblEj\Communication\Http\Util::HeaderRedirect("https://".$domainName.$requestUri, true, true, true);
         }
+
+        //delete old caches
+        WebIndex::UncacheStaticResources(null, true);
+
+        //first, let the router chain have a look at it.
+        //Maybe someone can handle it right away with no need for all the init stuff.
+        $preAppRouter = null;
+        $preappRawOutput = "";
+        $preappOutputHash = null;
+        HttpRouter::RouteThruNonAppCalls($requestUri, $preAppRouter, $preappRawOutput, $preappOutputHash);
+        if ($preAppRouter !== null)
+        {
+            WebIndex::CacheRawStaticResource($requestUri, $preappRawOutput, $preappOutputHash);//static resources such as images and scripts can be cached on disk
+            $exitCode = 0; //successful prerouting
+        } else {
+            $exitCode = -1;
+            if (getenv("WAFL_MAINTENANCE") == 1)
+            {
+                http_response_code(503);
+                require(__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Static".DIRECTORY_SEPARATOR."Maintenance.php");
+            }
+            $result = WebIndex::RunModularApplication($application, new ModularLoader(), $exitCode);
+            if ($result->Get_ResponseCode() == \DblEj\Communication\Http\Response::HTTP_OK_200)
+            {
+                WebIndex::CacheStaticResource($requestUri, $result);//static resources such as images and scripts can be cached on disk
+            }
+        }
+        exit($exitCode);
     }
 }
-
-//return the exit code
-exit($exitCode);
-
+catch (\DblEj\Communication\Http\Exception $e)
+{
+    if (\Wafl\Core::$RUNNING_APPLICATION->Get_Settings()->Get_Debug()->Get_DebugMode() && !\Wafl\Core::$RUNNING_APPLICATION->Get_Settings()->Get_Debug()->Get_SuppressErrors())
+    {
+        throw $e;
+    }
+    WebIndex::OutputHttpException($e, __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Static".DIRECTORY_SEPARATOR);
+}
+catch (\Exception $e)
+{
+    if (class_exists("\\Wafl\\Core") && \Wafl\Core::$RUNNING_APPLICATION->Get_Settings()->Get_Debug()->Get_DebugMode() && !\Wafl\Core::$RUNNING_APPLICATION->Get_Settings()->Get_Debug()->Get_SuppressErrors())
+    {
+        throw $e;
+    }
+    WebIndex::OutputException($e, __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Static".DIRECTORY_SEPARATOR);
+}
 ?>
