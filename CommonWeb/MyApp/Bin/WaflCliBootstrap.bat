8a4afd8c39267f8356bfa80b078768a23ffa1b70
Evan <Evan@EvanThinkpad440.ph.cox.net>
2015-05-10 17:09:56 -0700

    Fixed: Wasn't passing the environment argument when bootstrapping CLI applications in Windows

--- CommonWeb/MyApp/Bin/WaflCliBootstrap.bat
+++ CommonWeb/MyApp/Bin/WaflCliBootstrap.bat
@@ -10,9 +10,10 @@ set RUNPATH=%WAFL_PATH%
 GOTO FINISHED
 
 :PATHNOTSET
-for /f %%i in ('php ../Cli/GetWaflFolder.php') do set WAFL_PATH=%%i
+for /f %%i in ('php ../Cli/GetWaflFolder.php %1') do set WAFL_PATH=%%i
 @ECHO ActiveWAFL environment not set. Setting to %WAFL_PATH%
 set errorlevel=0
 set RUNPATH=%WAFL_PATH%
 
 :FINISHED
+@ECHO ActiveWAFL CLI Bootstrapped
