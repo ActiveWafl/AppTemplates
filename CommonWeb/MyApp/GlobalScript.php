22b55777d129bb18edada7547c33565ccaf07eef
Evan <Evan@EvanThinkpad440.ph.cox.net>
2015-05-13 13:39:39 -0700

    In GlobalScript.php we added routers for SitePageControls, Controls, and Models.  Removed routers for CaptchaImage, Glyphs, and Icons, all of which are done earlier in GlobalEventHandlers for pre-app routing.

--- CommonWeb/MyApp/GlobalScript.php
+++ CommonWeb/MyApp/GlobalScript.php
@@ -2,28 +2,25 @@
 
 use Wafl\Routers\Api,
     Wafl\Routers\ApplicationGlobal,
-    Wafl\Routers\CaptchaImage,
     Wafl\Routers\Webpage,
     Wafl\Routers\ControlResource,
     Wafl\Routers\Extensions,
-    Wafl\Routers\Glyphs,
-    Wafl\Routers\Icons,
     Wafl\Routers\PageIncludes,
     Wafl\Routers\SitewideControls,
     Wafl\Routers\WaflIncludes,
     Wafl\Routers\Fonts,
     Wafl\Util\HttpRouter;
 
-HttpRouter::AddRouter(new CaptchaImage());
 HttpRouter::AddRouter(new WaflIncludes());
 HttpRouter::AddRouter(new SitewideControls());
+HttpRouter::AddRouter(new SitepageControls());
+HttpRouter::AddRouter(new Controls());
 HttpRouter::AddRouter(new ApplicationGlobal());
 HttpRouter::AddRouter(new ControlResource());
 HttpRouter::AddRouter(new Extensions());
 HttpRouter::AddRouter(new Webpage());
-HttpRouter::AddRouter(new Fonts());
-HttpRouter::AddRouter(new Glyphs());
-HttpRouter::AddRouter(new Icons());
-HttpRouter::AddRouter(new Api());
+HttpRouter::AddRouter(new Models());
 HttpRouter::AddRouter(new PageIncludes());
+HttpRouter::AddRouter(new Fonts());
+HttpRouter::AddRouter(new Api());
 ?>
